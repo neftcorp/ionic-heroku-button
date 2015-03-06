752f653f1f743e3f47d9874ff0e3c538be273955
olegros <oleg.ros@neftcorp.com>
2015-03-06 10:57:10 -0800

    add data

--- www/js/services.js
+++ www/js/services.js
@@ -12,7 +12,8 @@ angular.module('starter.services', [])
     { id: 1, name: 'G.I. Joe' },
     { id: 2, name: 'Miss Frizzle' },
     { id: 3, name: 'Ash Ketchum' },
-    { id: 4, name: 'Jesper'}
+    { id: 4, name: 'Jesper'},
+    { id: 5, name: 'Cameron' }
   ];
 
   return {
